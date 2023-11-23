968a418ff0f3867bed25a3ce0c7fe42059e9e9aa
nam2k2k16fpt <ngohuunam2002@gmail.com>
2023-11-23 14:59:18 +0700

    update ui newArticle Setting

---
+++ realworld-app/src/components/screens/NewArticle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NoFooter from '../layout/NoFooter';
+
+const NewArticle = () => {
+    return (
+        <NoFooter>
+            This is my new article
+        </NoFooter>
+    );
+};
+
+export default NewArticle;
